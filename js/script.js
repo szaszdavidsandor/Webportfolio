5d072e6b9ab6f9bae43116fcb739520b40b8f17b
szaszdavidsandor <szasz.david.sandor@simonyiszki.org>
2026-06-22 11:11:56 +0200

    some repair

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('#navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
